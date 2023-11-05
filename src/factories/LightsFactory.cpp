ad1d2e91f99858679c6191c2eeaa0f4adc77ecfd
MegaMech <inkstudios1@hotmail.com>
2023-11-05 15:54:26 -0700

    fix

--- src/factories/LightsFactory.cpp
+++ src/factories/LightsFactory.cpp
@@ -8,26 +8,28 @@ void LightsCodeExporter::Export(std::ostream &write, std::shared_ptr<IParsedData
 
     write << "Lights1 " << symbol << "[] = gdSPDefLights1(\n";
 
-    // Diffuse
-    auto r = light.l[0].l.col[0];
-    auto g = light.l[0].l.col[1];
-    auto b = light.l[0].l.col[2];
+    // Ambient
+    auto r = light.a.l.col[0];
+    auto g = light.a.l.col[1];
+    auto b = light.a.l.col[2];
 
-    // Diffuse copy
-    auto r2 = light.l[0].l.colc[0];
-    auto g2 = light.l[0].l.colc[1];
-    auto b2 = light.l[0].l.colc[2];
+    // Diffuse
+    auto r2 = light.l[0].l.col[0];
+    auto g2 = light.l[0].l.col[1];
+    auto b2 = light.l[0].l.col[2];
 
     // Direction
     auto x = light.l[0].l.dir[0];
     auto y = light.l[0].l.dir[1];
     auto z = light.l[0].l.dir[2];
-    printf("HERE111");
+
+    printf("%X %X %X %X %X", r, g, b, r2, g2);
+
     write << fourSpaceTab;
     write << r << ", " << g << ", " << b << ",\n";
     write << fourSpaceTab;
     write << r2 << ", " << g2 << ", " << b2 << ", " << x << ", " << y << ", " << z << "\n";
-    printf("DONE");
+
     write << ");\n";
 }
 
@@ -43,7 +45,6 @@ void LightsBinaryExporter::Export(std::ostream &write, std::shared_ptr<IParsedDa
 std::optional<std::shared_ptr<IParsedData>> LightsFactory::parse(std::vector<uint8_t>& buffer, YAML::Node& node) {
     auto mio0 = node["mio0"].as<size_t>();
     auto offset = node["offset"].as<uint32_t>();
-    auto count = node["count"].as<size_t>();
     auto symbol = node["symbol"].as<std::string>();
 
     printf("HERE");
@@ -54,25 +55,25 @@ std::optional<std::shared_ptr<IParsedData>> LightsFactory::parse(std::vector<uin
     Lights1Raw lights;
     // Directional light
 
-    // Diffuse
+    // Ambient
     auto r = reader.ReadInt8();
     auto g = reader.ReadInt8();
     auto b = reader.ReadInt8();
-    auto a = reader.ReadInt8(); // unused
+
+    reader.Seek(5, LUS::SeekOffsetType::Current);
 
     // Diffuse copy
     auto r2 = reader.ReadInt8();
     auto g2 = reader.ReadInt8();
     auto b2 = reader.ReadInt8();
-    auto a2 = reader.ReadInt8(); // unused
+
+    reader.Seek(5, LUS::SeekOffsetType::Current);
 
     // Direction
     auto x = reader.ReadInt8();
     auto y = reader.ReadInt8();
     auto z = reader.ReadInt8();
-printf("HERE2");
     lights = {r, g, b, r2, g2, b2, x, y, z};
-printf("HERE3");
 
     return std::make_shared<LightsData>(lights);
 }
