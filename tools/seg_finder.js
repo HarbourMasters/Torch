a729b9fe5fd30c5f1e70b64dc257accfabbbbef6
KiritoDv <kiritodev01@gmail.com>
2023-11-15 15:00:31 -0600

    Removed unused stuff and added an example config file

--- tools/seg_finder.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const rom = fs.readFileSync('./baserom.us.z64')
-const segTable = 0x33b400
-
-for (let i = 0; i < 32; i++) {
-    const seg = rom.readUInt32BE(segTable + (i * 4))
-    console.log((seg & 0x00ffffff).toString(16))
-}
