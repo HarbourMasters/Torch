70800a914487440a7464ab826c4628f920ecc273
KiritoDv <kiritodev01@gmail.com>
2024-03-10 16:26:48 -0600

    Used to_string instead

--- src/factories/DisplayListFactory.cpp
+++ src/factories/DisplayListFactory.cpp
@@ -129,7 +129,7 @@ void DListCodeExporter::Export(std::ostream &write, std::shared_ptr<IParsedData>
     if (Companion::Instance->IsDebug()) {
         const auto sz = (sizeof(uint32_t) * cmds.size());
 
-        write << "// count: " << (sz / 8) << " DLists\n";
+        write << "// count: " << std::to_string(sz / 8) << " DLists\n";
         if (IS_SEGMENTED(offset)) {
             offset = SEGMENT_OFFSET(offset);
         }
