9d1c7ff3ece44f7d9e90f2abbb6828b406389ae3
KiritoDv <kiritodev01@gmail.com>
2024-03-27 09:41:55 -0600

    Fixed wrong return type

--- src/factories/sf64/ColPolyFactory.cpp
+++ src/factories/sf64/ColPolyFactory.cpp
@@ -53,7 +53,7 @@ ExportResult SF64::ColPolyCodeExporter::Export(std::ostream &write, std::shared_
     }
 
     write << "\n};\n";
-    int endOffset = ASSET_PTR(offset) + colpolys->mPolys.size() * sizeof(SF64::CollisionPoly);
+    uint32_t endOffset = ASSET_PTR(offset) + colpolys->mPolys.size() * sizeof(SF64::CollisionPoly);
     if (Companion::Instance->IsDebug()) {
         write << "// CollisionPoly count: " << colpolys->mPolys.size() << "\n";
         write << "// 0x" << std::uppercase << std::hex << endOffset << "\n";
@@ -99,7 +99,7 @@ std::optional<std::shared_ptr<IParsedData>> SF64::ColPolyFactory::parse(std::vec
     auto meshOffset = GetSafeNode<uint32_t>(node, "mesh_offset", offset + count * sizeof(SF64::CollisionPoly));
     for(int j = 0; j < meshCount; j++) {
         YAML::Node meshNode;
-        
+
         if(node["mesh_symbol"]) {
             auto meshSymbol = GetSafeNode<std::string>(node, "mesh_symbol");
             if (meshSymbol.find("OFFSET") == std::string::npos) {
@@ -119,7 +119,7 @@ std::optional<std::shared_ptr<IParsedData>> SF64::ColPolyFactory::parse(std::vec
         meshNode["offset"] = meshOffset;
 
         meshNode = Companion::Instance->AddAsset(meshNode).value();
-        
+
         meshNodes.push_back(meshNode);
         meshOffset += meshSize * sizeof(Vec3s);
     }
