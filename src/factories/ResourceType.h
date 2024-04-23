bf3a60cc2b1308dada0ca131fdd1eab8d1a9b209
inspectredc <inspectredc@gmail.com>
2024-04-23 15:18:35 +0100

    Trajectory Factory

--- src/factories/ResourceType.h
+++ src/factories/ResourceType.h
@@ -31,6 +31,7 @@ enum class ResourceType {
     Collision = 0x434F4C20,    // COL
     Movtex = 0x4D4F5654,       // MOVT
     MovtexQuad = 0x4D4F5651,   // MOVQ
+    Trajectory = 0x5452414A,   // TRAJ
 
     // MK64
     CourseVertex = 0x43565458, // CVTX
