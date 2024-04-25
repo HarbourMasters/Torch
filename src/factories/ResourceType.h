fa492d70d3823e555e448c4a863064e1fc51a6a0
inspectredc <inspectredc@gmail.com>
2024-04-25 15:52:32 +0100

    LevelScript Factory

--- src/factories/ResourceType.h
+++ src/factories/ResourceType.h
@@ -29,6 +29,7 @@ enum class ResourceType {
     Dictionary = 0x44494354,   // DICT
     GeoLayout = 0x47454F20,    // GEO
     Collision = 0x434F4C20,    // COL
+    LevelScript = 0x4C564C53,  // LVLS 
     MacroObject = 0x4D41434F,  // MACO
     Movtex = 0x4D4F5654,       // MOVT
     MovtexQuad = 0x4D4F5651,   // MOVQ
