a283c63d5244a733b96800ce7aebbd00af2e98b5
= <=>
2024-04-30 17:18:37 -0600

    Fix

--- src/utils/Decompressor.cpp
+++ src/utils/Decompressor.cpp
@@ -18,7 +18,6 @@ DataChunk* Decompressor::Decode(const std::vector<uint8_t>& buffer, const uint32
 
     const unsigned char* in_buf = buffer.data() + offset;
 
-                SPDLOG_INFO("Offset: 0x{:X}", offset);
     switch (type) {
         case CompressionType::MIO0: {
             mio0_header_t head;
