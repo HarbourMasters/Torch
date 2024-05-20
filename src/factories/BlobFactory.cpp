c479f40970dabe02fc42705f7e19864751f66118
MegaMech <MegaMech@users.noreply.github.com>
2024-05-20 10:51:23 -0600

    Update BlobFactory.cpp

--- src/factories/BlobFactory.cpp
+++ src/factories/BlobFactory.cpp
@@ -3,6 +3,21 @@
 #include "utils/Decompressor.h"
 #include <iomanip>
 
+ExportResult BlobHeaderExporter::Export(std::ostream &write, std::shared_ptr<IParsedData> raw, std::string& entryName, YAML::Node &node, std::string* replacement) {
+    const auto symbol = GetSafeNode(node, "symbol", entryName);
+    auto vtx = std::static_pointer_cast<VtxData>(raw)->mVtxs;
+    const auto offset = GetSafeNode<uint32_t>(node, "offset");
+
+    if(Companion::Instance->IsOTRMode()){
+        write << "static const ALIGN_ASSET(2) char " << symbol << "[] = \"__OTR__" << (*replacement) << "\";\n\n";
+        return std::nullopt;
+    }
+
+    write << "extern u8 *" << symbol << "[];\n";
+
+    return std::nullopt;
+}
+
 ExportResult BlobCodeExporter::Export(std::ostream &write, std::shared_ptr<IParsedData> raw, std::string& entryName, YAML::Node &node, std::string* replacement ) {
     auto symbol = GetSafeNode(node, "symbol", entryName);
     auto offset = GetSafeNode<uint32_t>(node, "offset");
