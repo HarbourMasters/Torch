b572c7d1f6236ac84a50b8f7d702780053d1ecec
MegaMech <MegaMech@users.noreply.github.com>
2024-05-20 10:51:15 -0600

    Update BlobFactory.h

--- src/factories/BlobFactory.h
+++ src/factories/BlobFactory.h
@@ -2,6 +2,11 @@
 
 #include "BaseFactory.h"
 #include "../types/RawBuffer.h"
+
+class BlobHeaderExporter : public BaseExporter {
+    ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
+};
+
 class BlobBinaryExporter : public BaseExporter {
     ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
 };
@@ -15,6 +20,7 @@ public:
     std::optional<std::shared_ptr<IParsedData>> parse(std::vector<uint8_t>& buffer, YAML::Node& data) override;
     inline std::unordered_map<ExportType, std::shared_ptr<BaseExporter>> GetExporters() override {
         return {
+            REGISTER(Header, BlobHeaderExporter)
             REGISTER(Binary, BlobBinaryExporter)
             REGISTER(Code, BlobCodeExporter)
         };
