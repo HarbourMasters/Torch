67e1670f9aef6e87d0ebe16d96753902b89376f7
MegaMech <MegaMech@users.noreply.github.com>
2024-06-08 07:03:56 -0600

    Update gbi-otr.h

--- src/n64/gbi-otr.h
+++ src/n64/gbi-otr.h
@@ -19,6 +19,7 @@
 #define G_MTX_OTR 0x36
 #define G_TEXRECT_WIDE 0x37
 #define G_FILLWIDERECT 0x38
+#define G_DL_OTR_INDEX 0x3D
 #define G_MOVEMEM_OTR_HASH 0x42
 
 // DL FLAGS
@@ -155,6 +156,13 @@ _DW({                                                                           
 #define gsSPDisplayListOTRHash(dl) \
     {{									\
         (_SHIFTL((G_DL_OTR_HASH), 24, 8) | _SHIFTL((0x00), 16, 8) | 			\
+         _SHIFTL((0), 0, 16)), 						\
+            (uintptr_t)(dl)						\
+    }}
+
+#define gsSPDisplayListOTRIndex(dl) \
+    {{                                  \
+        (_SHIFTL((G_DL_OTR_INDEX), 24, 8) | _SHIFTL((0x00), 16, 8) | 			\
          _SHIFTL((0), 0, 16)), 						\
             (uintptr_t)(dl)						\
     }}
