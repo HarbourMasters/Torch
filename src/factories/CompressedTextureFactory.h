77d2be8dc7180f7edbaf35c3cafa030ce0ae5fcd
inspectredc <inspectredc@gmail.com>
2024-11-26 15:59:38 +0000

    Compressed Texture Factory

--- src/factories/CompressedTextureFactory.h
+++ src/factories/CompressedTextureFactory.h
@@ -1,44 +1,46 @@
 #pragma once
 
 #include "BaseFactory.h"
+#include "utils/Decompressor.h"
 #include "utils/TextureUtils.h"
 
-class TextureData : public IParsedData {
+class CompressedTextureData : public IParsedData {
 public:
     TextureFormat mFormat;
     uint32_t mWidth;
     uint32_t mHeight;
     std::vector<uint8_t> mBuffer;
+    CompressionType mCompressionType;
 
-    TextureData(TextureFormat format, uint32_t width, uint32_t height, std::vector<uint8_t>& buffer) : mFormat(format), mWidth(width), mHeight(height), mBuffer(std::move(buffer)) {}
+    CompressedTextureData(TextureFormat format, uint32_t width, uint32_t height, std::vector<uint8_t>& buffer, CompressionType compressionType) : mFormat(format), mWidth(width), mHeight(height), mBuffer(std::move(buffer)), mCompressionType(compressionType) {}
 };
 
-class TextureHeaderExporter : public BaseExporter {
+class CompressedTextureHeaderExporter : public BaseExporter {
     ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
 };
 
-class TextureCodeExporter : public BaseExporter {
+class CompressedTextureCodeExporter : public BaseExporter {
     ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
 };
 
-class TextureBinaryExporter : public BaseExporter {
+class CompressedTextureBinaryExporter : public BaseExporter {
     ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
 };
 
-class TextureModdingExporter : public BaseExporter {
+class CompressedTextureModdingExporter : public BaseExporter {
     ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
 };
 
-class TextureFactory : public BaseFactory {
+class CompressedTextureFactory : public BaseFactory {
 public:
     std::optional<std::shared_ptr<IParsedData>> parse(std::vector<uint8_t>& buffer, YAML::Node& data) override;
     std::optional<std::shared_ptr<IParsedData>> parse_modding(std::vector<uint8_t>& buffer, YAML::Node& data) override;
     inline std::unordered_map<ExportType, std::shared_ptr<BaseExporter>> GetExporters() override {
         return {
-            REGISTER(Header, TextureHeaderExporter)
-            REGISTER(Binary, TextureBinaryExporter)
-            REGISTER(Code, TextureCodeExporter)
-            REGISTER(Modding, TextureModdingExporter)
+            REGISTER(Header, CompressedTextureHeaderExporter)
+            REGISTER(Binary, CompressedTextureBinaryExporter)
+            REGISTER(Code, CompressedTextureCodeExporter)
+            REGISTER(Modding, CompressedTextureModdingExporter)
         };
     }
     bool SupportModdedAssets() override { return true; }
