c5e1b1e7748d3ecb78c3913b5acec8afc6f10d8b
inspectredc <inspectredc@gmail.com>
2024-12-31 00:18:49 +0000

    Fix Crc after comptool

--- src/preprocess/CompTool.h
+++ src/preprocess/CompTool.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "lib/binarytools/BinaryWriter.h"
 #include <cstdint>
 #include <string>
 #include <vector>
@@ -15,4 +16,6 @@ public:
     static std::vector<uint8_t> Decompress(std::vector<uint8_t> rom);
 private:
     static uint32_t FindFileTable(std::vector<uint8_t>& rom);
+    static std::pair<uint32_t, uint32_t> CalculateCRCs(LUS::BinaryWriter& decompFile);
+    static inline const uint32_t sCrcSeed = 0xF8CA4DDC;
 };
