9ab2dee1f0568301d66304d3530331ab541d98f0
inspectredc <inspectredc@gmail.com>
2025-01-03 11:37:14 +0000

    Modding support

--- src/factories/fzerox/CourseFactory.h
+++ src/factories/fzerox/CourseFactory.h
@@ -27,7 +27,7 @@ typedef struct CourseRawData {
     ControlPoint controlPoint[64];
     int16_t bankAngle[64];
     int8_t pit[64];
-    int8_t boost[64];
+    int8_t dash[64];
     int8_t dirt[64];
     int8_t ice[64];
     int8_t jump[64];
@@ -42,7 +42,7 @@ typedef struct ControlPointInfo {
     ControlPoint controlPoint;
     int16_t bankAngle;
     int8_t pit;
-    int8_t boost;
+    int8_t dash;
     int8_t dirt;
     int8_t ice;
     int8_t jump;
@@ -86,20 +86,22 @@ class CourseCodeExporter : public BaseExporter {
     ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
 };
 
-// class CourseModdingExporter : public BaseExporter {
-//     ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
-// };
+class CourseModdingExporter : public BaseExporter {
+    ExportResult Export(std::ostream& write, std::shared_ptr<IParsedData> data, std::string& entryName, YAML::Node& node, std::string* replacement) override;
+};
 
 class CourseFactory : public BaseFactory {
 public:
     std::optional<std::shared_ptr<IParsedData>> parse(std::vector<uint8_t>& buffer, YAML::Node& data) override;
+    std::optional<std::shared_ptr<IParsedData>> parse_modding(std::vector<uint8_t>& buffer, YAML::Node& data) override;
     inline std::unordered_map<ExportType, std::shared_ptr<BaseExporter>> GetExporters() override {
         return {
             REGISTER(Code, CourseCodeExporter)
             REGISTER(Header, CourseHeaderExporter)
             REGISTER(Binary, CourseBinaryExporter)
-            // REGISTER(Modding, CourseModdingExporter)
+            REGISTER(Modding, CourseModdingExporter)
         };
     }
+    bool SupportModdedAssets() override { return true; }
 };
 } // namespace FZX
