85529e8536956391a616b24db88f1e422856cb44
coco875 <59367621+coco875@users.noreply.github.com>
2025-08-05 22:16:27 +0200

    ensure only one stb_image declaration

--- lib/n64graphics/n64graphics.c
+++ lib/n64graphics/n64graphics.c
@@ -9,7 +9,6 @@
 #define STBI_NO_LINEAR
 #define STBI_NO_HDR
 #define STBI_NO_TGA
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
