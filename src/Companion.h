ba4ef350077818a0081008fecedb604e115d338d
coco875 <59367621+coco875@users.noreply.github.com>
2025-08-05 19:09:46 +0200

    Update Companion.h

--- src/Companion.h
+++ src/Companion.h
@@ -135,9 +135,6 @@ public:
     explicit Companion(std::filesystem::path rom, const ArchiveType otr, const bool debug, const std::string& srcDir = "", const std::string& destPath = "") :
                        Companion(rom, otr, debug, false, srcDir, destPath) {}
 
-    explicit Companion(std::vector<uint8_t> rom, const ArchiveType otr, const bool debug, const std::string& srcDir = "", const std::string& destPath = "") :
-                       Companion(rom, otr, debug, false, srcDir, destPath) {}
-
     void Init(ExportType type);
 
     bool NodeHasChanges(const std::string& string);
