dd6eb7897a30b881c1f93e8a6f66b946e3b599a1
KiritoDv <kiritodev01@gmail.com>
2026-06-06 16:32:54 -0600

    Better exported some symbols to wasm

--- src/lib/web.cpp
+++ src/lib/web.cpp
@@ -45,9 +45,14 @@ EMSCRIPTEN_BINDINGS(Torch) {
         .constructor<std::vector<uint8_t>, ArchiveType, bool, bool, std::string, std::string>()
         .constructor<std::vector<uint8_t>, ArchiveType, bool, bool, std::string>()
         .constructor<std::vector<uint8_t>, ArchiveType, bool, bool>()
-        .function("Init", &Companion::Init)
+        .function("Init", optional_override([](Companion& self, ExportType type) {
+            self.Init(type);
+        }))
         .function("GetCartridge", &Companion::GetCartridge, allow_raw_pointers())
-        .function("Process", &Companion::Process)
+        .function("Process", optional_override([](Companion& self) {
+            std::atomic<size_t> dummy{0};
+            self.Process(dummy);
+        }))
         .function("GetRomData", &Companion::GetRomData, allow_raw_pointers());
 }
 #endif
